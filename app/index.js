561d032e9f9356a2ad99d6f0f5e99abb250fb2b9
Josephmx2019 <jlnoriega68@gmail.com>
2025-03-20 14:08:56 -0600

    Modifico Login y App

--- app/index.js
+++ app/index.js
@@ -1,16 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from '@react-navigation/stack'
+import { createNativeStackNavigator} from '@react-navigation/stack'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
 import DetailsScreen from "./screens/DetailsScreen";
+import { ActivityIndicator, View } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+    const [isLeggedIn, setIsLoggedIn] = useState(null); // Estado para almacenar si el usuario está autenticado
+
+    useEffect(() => {
+        // Función que revisa si hay un usuario guardado en AsyncStorage
+        const checkLoginStatus = async () => {
+            try{
+                const storedUsername = await AsyncStorage.getItem('username')
+                if (storedUsername){
+                    setIsLoggedIn(true) // Si hay un usuario guardado, el estado del login se actualiza
+                } else {
+                    setIsLoggedIn(false) // Si no hay usuario, redirigimos a login
+                }
+            } catch(error){
+                console.log("Error al verificar el estado de autenticación: ", error)
+                setIsLoggedIn(false)
+            }
+        }
+
+        checkLoginStatus()
+    }, [])
+
+    if(isLeggedIn === null){
+        // Mostrar un spinner de carga mientras se verifica el estado de autenticación
+        return (
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <ActivityIndicator size="large" color="#000ff" />
+            </View>
+        )
+    }
+
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login">
+            <Stack.Navigator initialRouteName={isLeggedIn ? "Home": "Login"}>
                 <Stack.Screen name="Login" component={LoginScreen}/>
                 <Stack.Screen name="Home" component={HomeScreen}/>
                 <Stack.Screen name="Details" component={DetailsScreen}/>
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,35 +1,70 @@
-import { useNavigation } from "@react-navigation/native"; // Cambia la importación
-import { Button, Text, TextInput, View } from "react-native"; // Importa desde react-native
 import React, { useState } from "react";
+import { View, TextInput, Button, Text, StyleSheet } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useNavigation } from "@react-navigation/native"; 
 
 export default function LoginScreen() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [errorMesage, setErrorMessage] = useState('')
     const navigation = useNavigation();
 
-    const handleLogin = () => {
-        if (username === 'admin' && password === 'password') {
-            navigation.navigate('Home'); // Navegar a HomeScreen
+    const handleLogin = async () => {
+        if (username && password) {
+            try{
+                // Guardar el nombre de usuario en AsyncStorage
+                await AsyncStorage.setItem('username', username)
+                navigation.replace('Home') // Reemplazar el stack para que no se pueda volver a Loggin
+            } catch(error){
+                console.log("Error al guardar el nombre de usuario: ", error)
+            }
         } else {
-            console.log('Credenciales incorrectas');
+            setErrorMessage("Por favor ingresa usuario y contraseña")
         }
     };
 
     return (
         <View>
-            <Text>Inicio de Sesión</Text>
+            <Text style={styles.container}>Iniciar sesión</Text>
+            {errorMesage ? <Text style={styles.error}>{errorMesage}</Text>: null}
             <TextInput
+                style={styles.input}
                 placeholder="Usuario"
                 value={username}
                 onChangeText={setUsername}
             />
             <TextInput
+                style={styles.input}
                 placeholder="Contraseña"
                 value={password}
                 onChangeText={setPassword}
                 secureTextEntry
             />
-            <Button title="Iniciar Sesión" onPress={handleLogin} />
+            <Button title="Iniciar sesión" onPress={handleLogin}/>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 20
+    },
+    header: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 12,
+        paddingHorizontal: 10,
+    },
+    error: {
+        color: 'red',
+        marginBottom: 10,
+    },
+})
